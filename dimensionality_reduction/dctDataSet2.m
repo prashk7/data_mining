e8118ba5f84fc98fbad96dd6e796bfdb06b946ff
PRASHANT PRABHU KARIYANNAVAR <54343627+prashk7@users.noreply.github.com>
2019-12-27 15:09:15 -0500

    DCT on data set2

---
+++ dimensionality_reduction/dctDataSet2.m
@@ -0,0 +1,18 @@
+data1 = dlmread('dist2_500_1.txt')
+data2 = dlmread('dist2_500_2.txt')
+
+dataSet1 = [data1;data2]
+X = dct2(dataSet1)
+[XX,ind] = sort(abs(X),'descend');
+i = 1;
+while norm(X(ind(1:i)))/norm(X) < 0.97  %Find how many DCT coefficients represent 97% of the energy in a sequence.
+   i = i + 1;
+end
+needed = i
+
+X(ind(needed+1:end)) = 0;
+xx = idct2(X);  %Reconstructing the signal and compare it to the original signal.
+
+plot([X;xx]')
+legend('Original',['Reconstructed, N = ' int2str(needed)], ...
+       'Location','SouthEast')
